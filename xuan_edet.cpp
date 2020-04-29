e04e8c388e51146f39a05f74057e45af0297244d
CraigWang1 <55901251+CraigWang1@users.noreply.github.com>
2020-04-29 13:25:10 -0700

    Update xuan_edet.cpp

--- xuan_edet.cpp
+++ xuan_edet.cpp
@@ -6,31 +6,31 @@
 
 #include <iostream>
 
-// helper function that resizes the image, processes the image, edits the scale as an output, and returns img_data std vector ready for model input
-std::vector<float> preprocess(cv::Mat image, float &scale, int image_size=512)
-{
-    image.convertTo(image, CV_32FC3);            //converts to float matrix so we can multiply and divide
-
+// helper function to resize image while preserving aspect ratio
+void resize(cv::Mat &image, int image_size, float &scale) {     //returns scale value for rescaling boxes back later
     int image_height = image.rows;     //initialize image_height variable for convenience
     int image_width = image.cols;      //initialize image_width variable for convenience
+
     int resized_height, resized_width;    //declare resized_height, resized_width variables to use in the if/else statements
 
     // calculates what height and width to resize to preserve height/width ratios
-    if (image_height > image_width)
-    {                 //if the image more tall than wide
+    if (image_height > image_width) {                 //if the image more tall than wide
         scale = (float)image_size / image_height;     //then the taller side will become the image_size (eg. 512), so the scale is 512/og_h
         resized_height = image_size;                  //downsize taller side to 512
         resized_width = (int)(image_width * scale);   //scale the width down by same scale as the height, preserving side ratios
     }
-    else
-    {                                                 //otherwise, this means the image is more wide than tall
+    else {                                            //otherwise, this means the image is more wide than tall
         scale = (float)image_size / image_width;      //repeat same procedures as above except for width
         resized_height = (int)(image_height * scale);
         resized_width = image_size;
     }
-
-    // cv matrix process stuff
     cv::resize(image, image, cv::Size(resized_width, resized_height));         //resize the image, keeping ratios
+}
+
+// helper function that resizes the image, processes the image, edits the scale as an output, and returns img_data std vector ready for model input
+std::vector<float> preprocess(cv::Mat image, int image_size) {
+    // cv matrix process stuff
+    image.convertTo(image, CV_32FC3);            //converts to float matrix so we can multiply and divide
     cv::Mat temp(image_size, image_size, CV_32FC3, cv::Scalar(128,128,128));   // makes temporary mat with shape (512,512,3) filled with 128s
     image.copyTo(temp(cv::Rect(0, 0, image.cols, image.rows)));                // pastes the image on top left corner (point 0, 0) of empty cv mat
 
@@ -49,19 +49,31 @@ Observation VisionService::findBinsML(cv::Mat img)
 {
     ROS_INFO("Starting machine learning detection for BIN.");
     log(img, 'd');
-    // assumes model is already loaded
-    // initialize the model's input and output tensors
+
+    //IMPORTANT: Edit architecture of model (eg. efficientdet-d3 -> phi=3)
+    int phi = 0;
+    int image_sizes[7] = {512, 640, 768, 896, 1024, 1152, 1280};     //image sizes that effdet uses
+    int image_size = image_sizes[phi];                                //takes the image size that our model uses
+    std::string classes[1] = {"bin"};        //list of classes
+
+    // inititialize the model's input and output tensors
     auto inpName = new Tensor(model, "input_1");
     auto out_boxes = new Tensor(model, "filtered_detections/map/TensorArrayStack/TensorArrayGatherV3");
     auto out_scores = new Tensor(model, "filtered_detections/map/TensorArrayStack_1/TensorArrayGatherV3");
     auto out_labels = new Tensor(model, "filtered_detections/map/TensorArrayStack_2/TensorArrayGatherV3");
 
-    // preprocess image
-    float scale;                                                //declare scale variable
-    std::vector<float> img_data = preprocess(img, scale, 512);  //process image for input
+    // read image
+    cv::Mat inp;                                              //input image that we process
+    img.copyTo(inp);
+    float scale;   //init scale variable that will be used to scale back out boxes to original image
+
+    //process image
+    resize(inp, image_size, scale);                             //resize, preserve aspect ratio
+    //underwaterEnhance(inp);                                   //phoebe enhance
+    std::vector<float> img_data = preprocess(inp, image_size);  //normalize image for model input
 
     // Put data in tensor.
-    inpName->set_data(img_data, {1,512,512,3});    //note: change this to 512 + (128*architecture) (eg. d0=512, d1=640, d2=768,etc.)
+    inpName->set_data(img_data, {1, image_size, image_size, 3});
 
     // run model
     model.run(inpName, { out_boxes, out_scores, out_labels });
@@ -72,17 +84,13 @@ Observation VisionService::findBinsML(cv::Mat img)
     auto labels = out_labels->get_data<int>();
 
     // scale output boxes back to original image
-    for (int i=0; i<boxes.size(); i++)
-    {
+    for (int i=0; i<boxes.size(); i++) {
         boxes[i] = boxes[i] / scale;
     }
 
-    std::string classes[1] = {"bin"};        //list of classes (used later on when writing visualization text)
     // iterate over results and draw the boxes for visualization!
-    for (int i=0; i<scores.size(); i++)
-    {
-        if (scores[i] > 0.3)
-        {
+    for (int i=0; i<scores.size(); i++) {
+        if (scores[i] > 0.3) {
             // extract output values
             float score = scores[i];
             int label = labels[i];
@@ -94,7 +102,7 @@ Observation VisionService::findBinsML(cv::Mat img)
                 ROS_INFO("Found bin.");
 
             // aesthetically visualize output box, label, and score
-            cv::Scalar color = {154, 209, 8};                                     //setup our color (blue,green,red)  (this is a turqoise color)
+            cv::Scalar color = {154, 209, 8};                                     //setup our color (blue,green,red)
             std::string text = classes[label] + '-' + std::to_string(score);      //setup our label: eg. "bin-0.9996"
             int baseline = 0;                                                     //baseline variable that the getTextSize function outputs
             cv::Size textSize = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseline);    //get our text size so we can be use it to draw aesthetic text
@@ -106,11 +114,10 @@ Observation VisionService::findBinsML(cv::Mat img)
             log(img, 'e');                                                                           //logs the image
             return Observation(score, (ymin+ymax)/2, (xmin+xmax)/2, 0);                              //returns observation(score, ycenter, xcenter, 0)
         }
-        else
-        {                  //since the outputs are already sorted by score
+        else {            //since the outputs are already sorted by score
             break;        //if it's lower than the thres then we know it's the last highest one,
         }                 //so we can afford to break out because all the other outputs will be below the threshold
     }
-    // if there was nothing over the threshold, then we just return all zeroes :(
-    return Observation(0, 0, 0, 0);
+
+    return 0;
 }
